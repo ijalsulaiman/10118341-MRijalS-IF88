7e80585e154e5165557cf2c0ff95e8aa7d4573f3
ijalsulaiman <rijals661@gmail.com>
2020-03-30 14:58:19 +0700

    first commit

--- provis8_praktikum1/src/kelasutama.java
+++ provis8_praktikum1/src/kelasutama.java
@@ -11,6 +11,7 @@
 public class kelasutama {
     public static void main(String[] args) {
         frameutama futama = new frameutama();
+        futama.tampil("10118341-Muhammad Rijal S");
         jfutama jf = new jfutama();
         jf.setVisible(true);
     }
