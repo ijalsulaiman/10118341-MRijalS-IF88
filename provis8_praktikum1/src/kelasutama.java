98d2bf9cb18e370645a455ec13c212cb8ca5a6f1
ijalsulaiman <rijals661@gmail.com>
2020-03-30 14:28:49 +0700

    "first commit"

---
+++ provis8_praktikum1/src/kelasutama.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Ijal Sulaiman
+ */
+public class kelasutama {
+    public static void main(String[] args) {
+        frameutama futama = new frameutama();
+        jfutama jf = new jfutama();
+        jf.setVisible(true);
+    }
+}
